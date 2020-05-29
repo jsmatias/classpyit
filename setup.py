f98e6f080c01c4fe84241413f00e0aa68d206ba0
Jean Matias <smatias.jean@gmail.com>
2020-05-29 10:30:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="classpyit-pkg-jsmatias", # Replace with your own username
+    name="classpyit", # Replace with your own username
     version="0.0.1",
     author="Jean Matias",
     author_email="smatias.jean@gmail.com",
